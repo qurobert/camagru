a9e4ac3870598862758cf498090fa4e8a3536a18
Alexis <alexis>
2024-04-29 23:25:56 +0200

    login, signup, isConnected work

---
+++ frontend/public/js/global.js
@@ -0,0 +1,76 @@
+// Globals
+export const url_api = 'http://localhost:3000';
+export function redirectTo(path) {
+	window.location.href = path;
+}
+
+// NAVIGATION LINKS
+export function refreshNavLink(isItConnected) {
+	const loginLink = document.getElementById('login-link');
+	const signupLink = document.getElementById('signup-link');
+	const profileLink = document.getElementById('profile-link');
+	const logoutLink = document.getElementById('logout-link');
+
+	profileLink.style.display = isItConnected ? 'block' : 'none';
+	logoutLink.style.display = isItConnected ? 'block' : 'none';
+	loginLink.style.display = isItConnected ? 'none' : 'block';
+	signupLink.style.display = isItConnected ? 'none' : 'block';
+}
+
+export function setToken(data) {
+	localStorage.setItem('access_token', data.access_token);
+	localStorage.setItem('refresh_token', data.refresh_token);
+}
+
+export function removeToken() {
+	localStorage.removeItem('access_token');
+	localStorage.removeItem('refresh_token');
+}
+
+export function getAccessToken() {
+	return localStorage.getItem('access_token');
+}
+
+export function getRefreshToken() {
+	return localStorage.getItem('refresh_token');
+}
+
+// AUTHENTICATION
+export function isConnected() {
+	const token = getAccessToken();
+	if (token) {
+		return fetch(url_api + '/users/me', {
+			method: 'GET',
+			headers: {
+				'Content-Type': "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		})
+			.then(response => response.json())
+			.then(data => {
+				sessionStorage.setItem('user', JSON.stringify(data.user));
+				return data.status === 200;
+			})
+			.catch(() => false)
+	}
+	return false;
+}
+
+export function logout() {
+	removeToken();
+	sessionStorage.removeItem('user');
+	refreshNavLink(false);
+	redirectTo('/');
+}
+
+export const logoutButton = document.getElementById('logout-button');
+
+if (logoutButton) {
+	logoutButton.addEventListener('click', logout);
+}
+
+export async function redirectProfileConnected() {
+	if (await isConnected()) {
+		redirectTo('profile');
+	}
+}
--- frontend/public/js/router.js
+++ frontend/public/js/router.js
@@ -1,3 +1,6 @@
+import {isConnected, redirectProfileConnected, redirectTo, refreshNavLink} from "./global.js";
+
+// Router
 const router = {
 	"/": {
 		pathView: createPathView("home"),
@@ -7,18 +10,31 @@ const router = {
 	},
 	"/login": {
 		pathView: createPathView("auth/login"),
+		pathScript: "./js/auth/login.js",
+		onLoadScript: async function () {
+			await redirectProfileConnected();
+		},
+		pathCss: "./css/auth.css",
 	},
 	"/signup": {
 		pathView: createPathView("auth/signup"),
+		pathScript: "./js/auth/signup.js",
+		onLoadScript: async function () {
+			await redirectProfileConnected();
+		},
+		pathCss: "./css/auth.css",
+	},
+	"/forgot-password": {
+		pathView: createPathView("auth/forgot-password"),
+		pathScript: "./js/auth/forgot-password.js",
 	},
 	"/profile": {
 		pathView: createPathView("profile"),
+		protected_route: true,
 	},
 	"/email-verification": {
 		pathView: createPathView("auth/email-verification"),
-	},
-	"logout": {
-		pathView: createPathView("auth/logout"),
+		// pathScript: "./js/auth/email-verification.js",
 	},
 	"/404": {
 		pathView: createPathView("error/404"),
@@ -29,35 +45,82 @@ function createPathView(filename) {
 	return "./pages/" + filename + ".html";
 }
 
-function getView(pathView) {
-	return fetch(pathView)
+// Utils
+function getFile(pathFile) {
+	return fetch(pathFile)
 		.then((response) => response.text())
-		.then((html) => html);
-
+		.then((content) => content);
 }
 
-async function loadView(path) {
+// Load App
+
+async function loadApp() {
+	await loadPage(window.location.pathname);
+
+	const isItConnected = await isConnected();
+	refreshNavLink(isItConnected);
+}
+
+async function loadPage(path) {
 	const route = router[path];
-	const view = await getView(route ? route.pathView : router["/404"].pathView);
+
+	unloadScript();
+	unloadCss();
+	if (route && route.protected_route && !(await isConnected())) {
+		redirectTo("/login");
+		return ;
+	}
+
+	const view = await getFile(route ? route.pathView : router["/404"].pathView);
 	const app = document.getElementById("content");
+	loadScript(route);
+	loadCss(route);
 	if (app)
 		app.innerHTML = view;
 }
 
-async function navigateTo(path) {
-	await loadView(path);
-	history.pushState({}, path, window.location.origin + path);
+function loadCss(route) {
+	const css = document.createElement("link");
+	css.rel = "stylesheet";
+	css.href = route.pathCss;
+	css.className = "css";
+	document.head.appendChild(css);
 }
+
+function loadScript(route) {
+	if (route && route.pathScript) {
+		const script = document.createElement("script");
+		script.src = route.pathScript;
+		script.type = "module";
+		if (route.onLoadScript)
+			script.onload = route.onLoadScript;
+		script.className = "script";
+		document.body.appendChild(script);
+	}
+}
+
+function unloadCss() {
+	document.querySelector(".css")?.remove();
+}
+
+function unloadScript() {
+	document.querySelector(".script")?.remove();
+}
+
+
+// Window Event
 window.addEventListener("popstate", async () => {
-	await loadView(window.location.pathname);
+	await loadPage(window.location.pathname);
 })
 
 window.addEventListener("DOMContentLoaded", async (e) => {
-	await loadView(window.location.pathname);
+	await loadApp();
 	document.querySelectorAll("a").forEach((a) => {
 		a.addEventListener("click", async (e) => {
 			e.preventDefault();
-			await navigateTo(a.getAttribute("href"));
+			const path = a.getAttribute("href");
+			await loadPage(path);
+			history.pushState({}, path, window.location.origin + path);
 		})
 	})
 })
