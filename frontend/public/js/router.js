ad35be5c16fada58a3c11865ee78be638c54fc5c
Alexis <alexis>
2024-04-29 20:53:17 +0200

    router update, it work so much better

--- frontend/public/js/router.js
+++ frontend/public/js/router.js
@@ -1,93 +1,63 @@
-class ContentManager {
+const router = {
+	"/": {
+		pathView: createPathView("home"),
+	},
+	"/home": {
+		pathView: createPathView("home"),
+	},
+	"/login": {
+		pathView: createPathView("auth/login"),
+	},
+	"/signup": {
+		pathView: createPathView("auth/signup"),
+	},
+	"/profile": {
+		pathView: createPathView("profile"),
+	},
+	"/email-verification": {
+		pathView: createPathView("auth/email-verification"),
+	},
+	"logout": {
+		pathView: createPathView("auth/logout"),
+	},
+	"/404": {
+		pathView: createPathView("error/404"),
+	},
+}
 
-	constructor(url) {
-		// load html
-		this._getHtmlFromUrl(url).then(this._loadHtml).catch((err) => {
-			console.error(err);
-			this._loadHtml("Error 404");
+function createPathView(filename) {
+	return "./pages/" + filename + ".html";
+}
+
+function getView(pathView) {
+	return fetch(pathView)
+		.then((response) => response.text())
+		.then((html) => html);
+
+}
+
+async function loadView(path) {
+	const route = router[path];
+	const view = await getView(route ? route.pathView : router["/404"].pathView);
+	const app = document.getElementById("content");
+	if (app)
+		app.innerHTML = view;
+}
+
+async function navigateTo(path) {
+	await loadView(path);
+	history.pushState({}, path, window.location.origin + path);
+}
+window.addEventListener("popstate", async () => {
+	await loadView(window.location.pathname);
+})
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+	await loadView(window.location.pathname);
+	document.querySelectorAll("a").forEach((a) => {
+		a.addEventListener("click", async (e) => {
+			e.preventDefault();
+			await navigateTo(a.getAttribute("href"));
 		})
-
-		// load js
-		const urlJs = "/js" + url + ".js";
-		// console.log(urlJs)
-		// try {
-		// 	this._fileExist(urlJs).then((exist) => {
-		// 		if (exist) {
-		// 			this._loadScript(urlJs);
-		// 		}
-		// 	}).catch();
-		// } catch (e) {
-		//
-		// }
-		// // unload js
-		// document.querySelectorAll(".script_js").forEach((script) => script.remove())
-	}
-
-	_getHtmlFromUrl(url) {
-		return new Promise((resolve, reject) => {
-			let xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === XMLHttpRequest.DONE) {
-					if (xhr.status === 200) {
-						resolve(xhr.responseText);
-					} else {
-						reject(new Error("Erreur de chargement du fichier HTML"));
-					}
-				}
-			};
-			xhr.open("GET", url, true);
-			xhr.setRequestHeader("Expires","0");
-			xhr.setRequestHeader("Pragma","no-cache");
-			xhr.setRequestHeader("Cache-Control","no-cache, no-store, must-revalidate");
-			xhr.send();
-		});
-	}
-
-	_loadHtml(html) {
-		const app = document.getElementById("content");
-		if (app)
-			app.innerHTML = html;
-	}
-
-	_fileExist(url) {
-		return new Promise((resolve, reject) => {
-			let xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === XMLHttpRequest.DONE) {
-					if (xhr.status === 200)
-						resolve(true);
-				}
-			};
-			xhr.open("HEAD", url, true);
-			xhr.send();
-		});
-	}
-
-	_loadScript(url) {
-		return new Promise((resolve, reject) => {
-			let script = document.createElement("script");
-			script.src = url;
-			script.onload = resolve;
-			script.onerror = reject;
-			script.class = "script_js";
-			document.head.appendChild(script);
-		});
-	}
-
-	_unloadScript(url) {
-		let script = document.getElementById(url);
-		if (script)
-			script.remove();
-	}
-}
-
-function changeContentWithUrl() {
-	let url = window.location.hash.substring(1);
-	if (url === "")
-		url = "/home";
-	new ContentManager(url);
-}
-
-window.addEventListener("hashchange", changeContentWithUrl)
-
-changeContentWithUrl();
+	})
+})
